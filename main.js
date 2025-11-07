652edd848128d81e8552ddb346fbb8259413fef2
JaelSantos01 <20223tn110@utez.edu.mx>
2025-11-07 15:01:41 -0600

    listo

--- main.js
+++ main.js
@@ -19,10 +19,9 @@ btnAdd.addEventListener('click', () => {
 
     db.put(tarea)
         .then(() => {
-            // Limpiar campos, pero NO refrescar la lista automáticamente.
-            // La lista sólo debe mostrarse cuando el usuario pulse "Listar Tareas".
             inputName.value = '';
             inputFecha.value = '';
+            listarTareas();
         })
         .catch(console.error);
 });
